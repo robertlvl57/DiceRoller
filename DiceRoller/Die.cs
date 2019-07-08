5182e50b469d9ea4a436e7bf8e459dc90f380372
robert lowry <lowryrob2001@gmail.com>
2019-07-08 13:32:58 -0700

    Add project files.

---
+++ DiceRoller/Die.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    /// <summary>
+    /// Represents a single
+    /// multi-sided die
+    /// </summary>
+    class Die
+    {
+        private byte faceValue;
+        private bool isHeld;
+        private byte numberOfSides;
+
+        public byte Roll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
